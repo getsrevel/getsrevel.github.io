bbb3896d4a1175d51cacabc0680797ae46521d22
Rasmus Stougaard <rst@eucnordvest.dk>
2019-11-12 00:33:26 +0100

    Add some content

---
+++ tech/javascript/plain-js/my-script.js
@@ -0,0 +1,16 @@
+// This file is included from the html file
+
+console.log("Hej i konsollen");
+
+function myFunction() {
+  console.log("AV!!!");
+}
+
+function myOtherFunction(){
+    console.log("Hej med dig");
+}
+
+
+function myThirdFunction(){
+  alert("Hvad har du gang i?");
+}
