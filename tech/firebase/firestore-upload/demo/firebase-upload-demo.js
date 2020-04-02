444b1edd8637000aac7c4d424342e3bf9717bf80
Rasmus Stougaard <rst@eucnordvest.dk>
2020-04-02 16:33:36 +0200

    publish

--- tech/firebase/firestore-upload/demo/firebase-upload-demo.js
+++ tech/firebase/firestore-upload/demo/firebase-upload-demo.js
@@ -1,29 +1,53 @@
 // Add the Firebase Admin SDK to Your Server
-let admin = require("firebase-admin");
+const firebase = require("firebase-admin");
+// Get the helper classes
+FieldValue = firebase.firestore.FieldValue;
+Timestamp = firebase.firestore.Timestamp;
+
+const moment = require("moment");
 
 // Import crecential for the service account
-let serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 // Initialize the default app
-let app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+const app = firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
   databaseURL: "https://coldhawaiiweather.firebaseio.com"
 });
 
-let db = admin.firestore();
-
-// Generate some fake weather data
-const weatherData = {
-  time: Date.now(),
-  windSpeed: getRandomInt(15, 3),
-  windDir: getRandomInt(0, 359),
-};
-
-// Add document to Firestore
-let addDoc = db.collection("weather").add(weatherData);
+const db = firebase.firestore();
 
 // Helper - Generates random integer values 
 function getRandomInt(max, min) {
   const diff = max - min;
   return Math.floor(Math.random() * Math.floor(diff) + min);
 }
+
+async function storeData() {
+
+  // Set time of update
+  const updateTime = Timestamp.now();
+
+  // Generate some fake weather data
+  const windData = {
+    time: updateTime,
+    windSpeed: getRandomInt(15, 3),
+    windDir: getRandomInt(0, 359),
+  };
+
+  // Document ID should be todays date
+  const today = moment(updateTime.toDate());
+  const docPath = `weather/${today.format("YYYY-MM-DD")}`;
+  const docRef = db.doc(docPath);
+
+  const data = {
+    lastUpdate: updateTime,
+    windMeasurements: FieldValue.arrayUnion(windData),
+  };
+  
+  const options = { merge: true }
+  await docRef.set(data, options);
+
+}
+
+storeData();
