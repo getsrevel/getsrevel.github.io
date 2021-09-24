bbf0fd0d872285377ad7243f0245939764e5405a
Rasmus Stougaard <rst@eucnordvest.dk>
2021-09-24 12:16:41 +0200

    publish

--- tech/p5js/ball-bounce/demo/sketch.js
+++ tech/p5js/ball-bounce/demo/sketch.js
@@ -32,29 +32,37 @@ function draw() {
 
   rect(0, 0, width, height);
 
-  // opret variablen d og tildel værdien 120
-  let d = 120;
+  // opret variabler til radius og diameter
+  let r = 60;
+  let d = r * 2;
 
   // tegn en cirkel med centrum i (x, y) og diameter d
   circle(x, y, d);
 
   // hvis x er større end bredden af lærredet
   // så sæt xSpeed til -xSpeed
-  if (x > width - d / 2) {
+  let isPastRightSide = width < x + r
+  if (isPastRightSide) {
     changeColor();
     xSpeed = -xSpeed;
   }
 
-  if (x < 0 + d / 2) {
+  let isPastLeftSide = 0 > x - r
+  if (isPastLeftSide) {
     changeColor();
     xSpeed = -xSpeed;
   }
 
-  if (y > height - d / 2 || y < 0 + d / 2) {
+  let isBelowBottom = height < y + r
+  let isAboveTop = 0 > y - r
+  if (isBelowBottom || isAboveTop) {
     changeColor();
     ySpeed = -ySpeed;
   }
 
   x = x + xSpeed;
-  y = y + ySpeed;
+
+  // samme som 
+  // y = y + ySpeed;
+  y += ySpeed;
 }
