9604b8d6ce2b9e44eec9e0aad3adb12c27395ee8
Rasmus Stougaard <rst@eucnordvest.dk>
2021-11-04 12:38:16 +0100

    publish

--- tech/p5js/circle-rain/demo/sketch.js
+++ tech/p5js/circle-rain/demo/sketch.js
@@ -1,9 +1,9 @@
 const ballCount = 100
-let ds = new Array(ballCount);
-let xs = new Array(ballCount);
-let ys = new Array(ballCount);
+let diameters = new Array(ballCount);
+let xPositions = new Array(ballCount);
+let yPositions = new Array(ballCount);
 
-let ySpeed = new Array(ballCount);
+let ySpeeds = new Array(ballCount);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,17 +14,17 @@ function draw() {
   background(220);
 
   for (let i = 0; i < ballCount; i++) {
-    circle(xs[i], ys[i], ds[i])
-    ys[i] += ySpeed[i];
+    circle(xPositions[i], yPositions[i], diameters[i])
+    yPositions[i] += ySpeeds[i];
   }
 }
 
 function reset(maxSize) {
   for (let i = 0; i < ballCount; i++) {
-    ds[i] = random(0, maxSize);
-    ySpeed[i] = random(0.01, 10);
-    xs[i] = random(0, width);
-    ys[i] = random(0, height) - height;
+    diameters[i] = random(0, maxSize);
+    ySpeeds[i] = random(0.01, 10);
+    xPositions[i] = random(0, width);
+    yPositions[i] = random(0, height) - height;
   }  
 }
 
